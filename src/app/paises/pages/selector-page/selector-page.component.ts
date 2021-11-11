31ceec653a83c8d2bc49f75d0868dbb6d33e096e
Fabio <fabioaoad@gmail.com>
2021-11-11 17:30:57 -0300

    Primer selector, continentes - Selectores - seccion 19 - apartado 7

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {PaisesService} from "../../services/paises.service";
 
 @Component({
   selector: 'app-selector-page',
@@ -13,9 +14,16 @@ export class SelectorPageComponent implements OnInit {
     region: ['', Validators.required]
   });
 
-  constructor( private fb: FormBuilder) { }
+  // llenar selectores
+
+  regiones: string[] = [];
+
+  constructor( private fb: FormBuilder,
+               private pasisesService: PaisesService ) { }
 
   ngOnInit(): void {
+
+    this.regiones = this.pasisesService.regiones;
   }
 
 
