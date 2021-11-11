396a97c718fd63ec8159e0640ce2b1186790cdf2
Fabio <fabioaoad@gmail.com>
2021-11-11 17:57:40 -0300

    Selector anidado. Al seleccionar el continente en el otro selector se cargan los paises - Selectores - seccion 19 - apartado 8

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {PaisesService} from "../../services/paises.service";
+import {PaisesSmall} from "../../interfaces/paises.interface";
 
 @Component({
   selector: 'app-selector-page',
@@ -11,12 +12,14 @@ import {PaisesService} from "../../services/paises.service";
 export class SelectorPageComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
-    region: ['', Validators.required]
+    region: ['', Validators.required],
+    pais: ['', Validators.required],
   });
 
   // llenar selectores
 
   regiones: string[] = [];
+  paises: PaisesSmall[] = [];
 
   constructor( private fb: FormBuilder,
                private pasisesService: PaisesService ) { }
@@ -24,6 +27,17 @@ export class SelectorPageComponent implements OnInit {
   ngOnInit(): void {
 
     this.regiones = this.pasisesService.regiones;
+
+    //cuando cambie la region(continente)
+    this.miFormulario.get('region')?.valueChanges
+      .subscribe( region => {
+        console.log(region);
+        this.pasisesService.getPaisesPorRegion( region )
+          .subscribe( paises => {
+            this.paises = paises;
+            console.log(paises);
+          })
+      });
   }
 
 
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+import {PaisesSmall} from "../interfaces/paises.interface";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
 
+  private baseUrl: string = 'https://restcountries.com/v3.1/'
   private _regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
 
 
   get regiones(): string[]{
     return [ ...this._regiones ];
   }
-  constructor() { }
+  constructor( private  http: HttpClient) { }
+
+
+  getPaisesPorRegion( region: string ): Observable<PaisesSmall[]>{
+    const url : string = `${ this.baseUrl }/region/${ region }?fields=cca3,name`
+    return this.http.get<PaisesSmall[]>( url );
+  }
+
 }
